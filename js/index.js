d57f2e12e04f14fe3802d885d161402584e7a70b
CesarRam <Cesar.Ram.30020@gmail.com>
2023-12-21 13:03:14 -0600

    Pruebas

--- js/index.js
+++ js/index.js
@@ -1,10 +1,17 @@
-const onload = () => {
-    let body = "";
+let body = "";
+let cont = 0;
 
+const onload = () => {
     for (let i = 1; i < 6; i++) {
         body += "<h"+i+"> Elemento "+i+"</h"+i+">";
     }
     body += "<hr>";
 
+    document.getElementById("cuerpo").innerHTML = body;
+};
+
+const agregarTexto = () => {
+    cont += 1;
+    body += `Elemento extra no ${cont}`;
     document.getElementById("cuerpo").innerHTML = body;
 };
